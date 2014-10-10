8500c135e34e0234c07bc07ad51f5f35d1d8379d
Tarkeshwar Singh <tarkeshwar.iitgn@gmail.com>
2014-10-11 00:20:24 +0530

    Removed consoling every datapoint

--- controllers/deviceidentity.js
+++ controllers/deviceidentity.js
@@ -115,9 +115,8 @@ exports.getroomlist = function(req, res) {
       for (var i = roomlist.length - 1; i >= 0; i--) {
         roomresponselist.push(roomlist[i].RoomId);
       if(i==0){
-        console.log(roomresponselist);
       res.json(roomresponselist);
-      console.log(roomlist);};
+    };
       }
       };
 
